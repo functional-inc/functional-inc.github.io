157990f73488ea1b35817c00830138dbb654d441
shu-pf <shugoena@gmail.com>
2021-04-26 13:53:01 +0900

    stylelintの導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,48 @@
 export default {
   ssr: false,
-  css: ["~/assets/css/queries", "~/assets/css/style"],
+  css: ['~/assets/css/queries', '~/assets/css/style'],
   head: {
-    title: "トップページ | 株式会社functional",
+    title: 'トップページ | 株式会社functional',
     htmlAttrs: {
-      lang: "ja",
+      lang: 'ja'
     },
     meta: [
-      { charset: "utf-8" },
+      { charset: 'utf-8' },
       {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1, shrink-to-fit=no",
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, shrink-to-fit=no'
       },
       {
-        hid: "description",
-        name: "description",
-        content: "サイトの説明文",
+        hid: 'description',
+        name: 'description',
+        content: 'サイトの説明文'
       },
       {
-        property: "og:type",
-        content: "website",
+        property: 'og:type',
+        content: 'website'
       },
       {
-        property: "og:url",
-        content: "https://functional-inc.com",
+        property: 'og:url',
+        content: 'https://functional-inc.com'
       },
       {
-        property: "og:image",
-        content: "./img/logo-wt.png",
+        property: 'og:image',
+        content: './img/logo-wt.png'
       },
       {
-        property: "og:title",
-        content: "トップページ | 株式会社functional",
+        property: 'og:title',
+        content: 'トップページ | 株式会社functional'
       },
       {
-        property: "og:description",
-        content: "株式会社functionalのコーポレートサイト",
-      },
-    ],
+        property: 'og:description',
+        content: '株式会社functionalのコーポレートサイト'
+      }
+    ]
   },
-  buildModules: ["@nuxtjs/eslint-module", "@nuxt/typescript-build"],
-  plugins: ["~/plugins/bootstrap-vue.ts"],
-};
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/stylelint-module',
+    '@nuxt/typescript-build'
+  ],
+  plugins: ['~/plugins/bootstrap-vue.ts']
+}
